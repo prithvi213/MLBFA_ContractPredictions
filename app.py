3851567e0b260a171d97547f425b624e0cf23374
Prithvi <prithvia05@Prithvis-MacBook-Pro-2.local>
2024-11-02 15:14:26 -0700

    Cleaned training dataset entirely

--- app.py
+++ app.py
@@ -1,10 +1,46 @@
 import streamlit as st
 import pandas as pd
-from pybaseball import fangraphs
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+import re
+
+# Split Position by Slash Token
+def split_positions(position):
+    positions = re.split(r'\/', position[0])
+    return [pos for pos in positions if pos]
 
 # Create the dataset using Pandas
-free_agents = pd.read_csv('data/export_table#table_11_1_2024.csv')
-player_names = list(free_agents['PLAYER (50)'])
+previous_free_agents = pd.read_csv('data/export_table#table_11_2_2024.csv')
+
+# Cleaning the training dataset
+previous_free_agents = previous_free_agents.drop(0)
+previous_free_agents = previous_free_agents.drop(columns=['TEAMFROM', 'TEAMTO', 'WAR', 'YOE', 'ARMBAT/THROW'])
+
+index_of_player1972 = previous_free_agents[previous_free_agents['RANK'] == 'PLAYER (1972)'].index[0]
+previous_free_agents = previous_free_agents.iloc[:index_of_player1972 - 1]
+previous_free_agents.dropna(subset=['POS'], inplace=True)
+previous_free_agents[['YEAR', 'YRS']] = previous_free_agents[['YEAR', 'YRS']].astype(int)
+previous_free_agents.fillna('$0', inplace=True)
+previous_free_agents[['VALUE', 'AAV']] = previous_free_agents[['VALUE', 'AAV']].replace({'\$': '', ',': ''}, regex=True).astype(int)
+previous_free_agents = previous_free_agents[(previous_free_agents['YRS'] > 0) &
+                                            (previous_free_agents['VALUE'] > 0) &
+                                            (previous_free_agents['AAV'] > 0) &
+                                            (previous_free_agents['YEAR'] > 2011)]
+
+previous_free_agents['POS'] = previous_free_agents['POS'].apply(lambda pos: [pos])
+previous_free_agents['POS'] = previous_free_agents['POS'].apply(split_positions)
+previous_free_agents['POS'] = previous_free_agents['POS'].apply(lambda pos_list: list(set(pos_list)))
+previous_free_agents['POS'][1] = ['TWP']
+
+previous_free_agents['AGE'] = pd.to_numeric(previous_free_agents['AGE'], errors='coerce')
+previous_free_agents['AGE'] = np.round(previous_free_agents['AGE'] - 1)
+previous_free_agents['AGE'] = previous_free_agents['AGE'].astype(int)
+
+print(previous_free_agents)
+
+current_free_agents = pd.read_csv('data/export_table#table_11_1_2024.csv')
+player_names = list(current_free_agents['PLAYER (50)'])
 st.title("MLB Free Agent Contract Predictor")
 
 # Player Dropdown for Free Agent
