94fa32af7a531a3d9b63a738a056d5bfedbb7cb3
Prithvi <prithvia05@eduroam-169-233-246-163.ucsc.edu>
2024-11-01 20:18:34 -0700

    Created a dataset of all the MLB free agents and in streamlit app have a dropdown for each of the upcoming free agents this offseason

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pandas as pd
+from pybaseball import fangraphs
 
+# Create the dataset using Pandas
+free_agents = pd.read_csv('data/export_table#table_11_1_2024.csv')
+player_names = list(free_agents['PLAYER (50)'])
 st.title("MLB Free Agent Contract Predictor")
-st.write("Hello World")
+
+# Player Dropdown for Free Agent
+player = st.selectbox('Select a FA', player_names)
