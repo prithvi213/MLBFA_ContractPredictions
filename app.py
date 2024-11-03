513891b501a6c5a8cfa72806585d5fc7bd21d96f
Prithvi <prithvia05@Prithvis-MacBook-Pro-2.local>
2024-11-02 18:56:44 -0700

    Adjust previous FA contracts to present-day value using league average salaries each year

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from pybaseball import fg_batting_data, fg_pitching_data
 import numpy as np
 import re
 
@@ -9,6 +10,25 @@ import re
 def split_positions(position):
     positions = re.split(r'\/', position[0])
     return [pos for pos in positions if pos]
+
+# League average MLB Salary in millions to help adjust for inflation
+lg_avg = {
+    2012: 3.21,
+    2013: 3.39,
+    2014: 3.69,
+    2015: 3.84,
+    2016: 4.38,
+    2017: 4.45,
+    2018: 4.41,
+    2019: 4.38,
+    2020: 4.43,
+    2021: 4.17,
+    2022: 4.41,
+    2023: 4.9
+}
+
+batting_data = fg_batting_data(2024, 2024)
+print(batting_data[['Name', 'Team', 'WAR']].head()) 
 
 # Create the dataset using Pandas
 previous_free_agents = pd.read_csv('data/export_table#table_11_2_2024.csv')
@@ -22,22 +42,30 @@ previous_free_agents = previous_free_agents.iloc[:index_of_player1972 - 1]
 previous_free_agents.dropna(subset=['POS'], inplace=True)
 previous_free_agents[['YEAR', 'YRS']] = previous_free_agents[['YEAR', 'YRS']].astype(int)
 previous_free_agents.fillna('$0', inplace=True)
-previous_free_agents[['VALUE', 'AAV']] = previous_free_agents[['VALUE', 'AAV']].replace({'\$': '', ',': ''}, regex=True).astype(int)
+previous_free_agents[['VALUE', 'AAV']] = previous_free_agents[['VALUE', 'AAV']].replace({'\$': '', ',': ''}, regex=True).astype(float)
 previous_free_agents = previous_free_agents[(previous_free_agents['YRS'] > 0) &
                                             (previous_free_agents['VALUE'] > 0) &
                                             (previous_free_agents['AAV'] > 0) &
-                                            (previous_free_agents['YEAR'] > 2011)]
+                                            (previous_free_agents['YEAR'] > 2012)]
 
 previous_free_agents['POS'] = previous_free_agents['POS'].apply(lambda pos: [pos])
 previous_free_agents['POS'] = previous_free_agents['POS'].apply(split_positions)
 previous_free_agents['POS'] = previous_free_agents['POS'].apply(lambda pos_list: list(set(pos_list)))
-previous_free_agents['POS'][1] = ['TWP']
+previous_free_agents.loc[1, 'POS'] = ['TWP']
 
 previous_free_agents['AGE'] = pd.to_numeric(previous_free_agents['AGE'], errors='coerce')
 previous_free_agents['AGE'] = np.round(previous_free_agents['AGE'] - 1)
 previous_free_agents['AGE'] = previous_free_agents['AGE'].astype(int)
 
-print(previous_free_agents)
+previous_free_agents['VALUE'] = previous_free_agents['VALUE'] * (4.98 / previous_free_agents['YEAR'].apply(lambda c: lg_avg.get(c - 1)))
+previous_free_agents['VALUE'] = np.round(previous_free_agents['VALUE'])
+previous_free_agents['VALUE'] = previous_free_agents['VALUE'].astype(int)
+
+previous_free_agents['AAV'] = previous_free_agents['VALUE'] / previous_free_agents['YRS']
+previous_free_agents['AAV'] = np.round(previous_free_agents['AAV'])
+previous_free_agents['AAV'] = previous_free_agents['AAV'].astype(int)
+
+print(previous_free_agents.head(50))
 
 current_free_agents = pd.read_csv('data/export_table#table_11_1_2024.csv')
 current_free_agents = current_free_agents.drop(columns=['YOE', 'ARMBAT/THROW', 'TEAM', 'PREV AAV', 'TYPE', 'MARKET VALUE', 'WAR'])
@@ -45,9 +73,9 @@ current_free_agents['AGE'] = pd.to_numeric(current_free_agents['AGE'], errors='c
 current_free_agents['AGE'] = np.round(current_free_agents['AGE'] + 0.2)
 current_free_agents['AGE'] = current_free_agents['AGE'].astype(int)
 
-
-print(current_free_agents)
 player_names = list(current_free_agents['PLAYER (50)'])
+#print(current_free_agents)
+
 st.title("MLB Free Agent Contract Predictor")
 
 # Player Dropdown for Free Agent
